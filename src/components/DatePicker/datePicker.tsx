6b7f4f2a9e07535a686d24cbfb9ee667cf97eb10
Shanklesh <shanklesh@divami.com>
2022-07-25 11:11:50 +0530

    material UI import changes

--- src/components/DatePicker/datePicker.tsx
+++ src/components/DatePicker/datePicker.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TextField } from "@material-ui/core";
+import { TextField } from "@mui/material";
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
