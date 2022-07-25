da4322cfff28a528916e0603834750d9cdb1b65b
Shanklesh <shanklesh@divami.com>
2022-07-25 08:01:53 +0530

    date picker implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import "./App.scss";
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import { DatePickerWrapper } from './wrappers/DatePickerWrapper';
+import { InputNumberWrapper } from './wrappers/InputNumberWrapper';
+import { InputTextWrapper } from './wrappers/InputTextWrapper';
 import { CheckboxWrapper } from "./wrappers/CheckboxWrapper";
-import { InputNumberWrapper } from "./wrappers/InputNumberWrapper";
-import { InputTextWrapper } from "./wrappers/InputTextWrapper";
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>
-          <Link to="/text">Text</Link>
-        </li>
-        <li>
-          <Link to="/number">Number</Link>
-        </li>
-        <li>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/text">Text</Link>
+          </li>
+          <li>
+            <Link to="/number">Number</Link>
+          </li>
+          <li>
+            <Link to="/datePicker">Date Picker</Link>
+          </li>
+          <li>
           <Link to="/checkbox">Checkbox</Link>
         </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<InputTextWrapper />}></Route>
-        <Route path="/text" element={<InputTextWrapper />}></Route>
-        <Route path="/number" element={<InputNumberWrapper />}></Route>
-        <Route path="/checkbox" element={<CheckboxWrapper />}></Route>
-      </Routes>
-    </div>
+        </ul>
+        <Routes>
+          <Route path='/' element={<InputTextWrapper />}></Route>
+          <Route path='/text' element={<InputTextWrapper />}></Route>
+          <Route path='/number' element={<InputNumberWrapper />}></Route>
+          <Route path='/datePicker' element={<DatePickerWrapper />}></Route>
+          <Route path="/checkbox" element={<CheckboxWrapper />}></Route>
+        </Routes>
+      </div>
   );
 };
 
